80e0e24503971e04eb8f870e660d9db4b53eeb1d
DrCord <cord42@gmail.com>
2018-01-25 13:18:19 -0800

    add numbersList stylesheet, minimally style numbrsList component and refactor html for component to work better with expected functionality and layout

--- src/App.js
+++ src/App.js
@@ -19,7 +19,9 @@ class App extends Component {
         <p className="App-intro">
           Click the start button to begin. The app will randomly select a number from 1-100 (inclusive) and you will attempt to guess it. The app will let you know if your guess is close to the correct number(HOT) or far away from the correct number (COLD).
         </p>
-        <NumbersList />
+        <div className="App-body">
+          <NumbersList />
+        </div>
       </div>
     );
   }
--- src/components/numbers-list.js
+++ src/components/numbers-list.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
+import './numbers-list.css';
+
 export default function NumbersList(props) {
     var N = 100;
     let numbers = Array.apply(null, {length: N}).map(Number.call, Number);
 
     const numbersList = numbers.map((number, index) =>
         <li key={index+1}>
-            {number+1}
+            <button>{number+1}</button>
         </li>
     );
 
